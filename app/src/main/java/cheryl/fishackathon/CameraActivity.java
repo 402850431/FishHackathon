74b0a6adafa0f2225dc6462eb25b2362bc6d45cc
cherylgao <402850431@s02.tku.edu.tw>
2018-02-03 21:49:34 +0800

    okok

--- app/src/main/java/cheryl/fishackathon/CameraActivity.java
+++ app/src/main/java/cheryl/fishackathon/CameraActivity.java
@@ -1,13 +1,283 @@
 package cheryl.fishackathon;
 
+import android.Manifest;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.mvc.imagepicker.ImagePicker;
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import static com.yalantis.ucrop.util.FileUtils.getDataColumn;
+import static com.yalantis.ucrop.util.FileUtils.isDownloadsDocument;
+import static com.yalantis.ucrop.util.FileUtils.isExternalStorageDocument;
+import static com.yalantis.ucrop.util.FileUtils.isMediaDocument;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private static final int REQUEST_SELECT_PICTURE = 0x01;
+    protected static final int REQUEST_STORAGE_WRITE_ACCESS_PERMISSION = 102;
+    AlertDialog mAlertDialog;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+//        ImagePicker.setMinQuality(600, 600);
+
     }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap bitmap = ImagePicker.getImageFromResult(this, requestCode, resultCode, data);
+//        imageView.setImageBitmap(bitmap);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_SELECT_PICTURE) {
+
+                final Uri selectedUri = data.getData();
+                Log.e("selectedUri",selectedUri.toString());
+
+                try {
+                    Glide.with(CameraActivity.this)
+                            .load(new File(getPath(selectedUri)))
+//                        .apply(RequestOptions.centerCropTransform())
+                            .into(imageView);
+                }catch (Exception e){
+                    Log.e(">>asdf e.getMessage()",e.getMessage());
+                }
+                if (selectedUri != null) {
+                    startCropActivity(selectedUri);
+                } else {
+                    Toast.makeText(CameraActivity.this, "無法顯示選擇的圖片", Toast.LENGTH_SHORT).show();
+                }
+            } else if (requestCode == UCrop.REQUEST_CROP) {
+                handleCropResult(data);
+            }
+        }
+        if (resultCode == UCrop.RESULT_ERROR) {
+            handleCropError(data);
+        }
+    }
+
+    private void startCropActivity(@NonNull Uri uri) {
+        String destinationFileName = "hello.jpg";
+        UCrop.of(uri, Uri.fromFile(new File(getCacheDir(), destinationFileName)))
+                .withAspectRatio(16, 9)
+                .withMaxResultSize(1600, 900)
+                .start(CameraActivity.this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public String getPath(Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(this, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(this, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(this, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(this, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+
+    private void handleCropResult(@NonNull Intent result) {
+        final Uri resultUri = UCrop.getOutput(result);
+        if (resultUri != null) {
+
+//            Glide.with(this)
+//                    .load(new File(resultUri.getPath()))
+//                    .apply(RequestOptions.centerCropTransform())
+//                    .into(imageView);
+            saveCroppedImage(resultUri);
+        } else {
+            Toast.makeText(this, "無法顯示選擇的圖片", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void saveCroppedImage(Uri imageUri) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    "需要儲存權限",
+                    REQUEST_STORAGE_WRITE_ACCESS_PERMISSION);
+        } else {
+            if (imageUri != null&& imageUri.getScheme().equals("file")) {
+                try {
+                    copyFileToDownloads(imageUri);
+                } catch (Exception e) {
+                    Log.e(">>>>>>>>>e.getMessage()",e.getMessage());
+                }
+            } else {
+                Toast.makeText(this, "unexpected error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    protected void requestPermission(final String permission, String rationale, final int requestCode) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            showAlertDialog("需要儲存權限", rationale,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(CameraActivity.this,
+                                    new String[]{permission}, requestCode);
+                        }
+                    }, "確認", null, "取消");
+        } else {
+            ActivityCompat.requestPermissions(CameraActivity.this, new String[]{permission}, requestCode);
+        }
+    }
+
+    protected void showAlertDialog(@Nullable String title, @Nullable String message,
+                                   @Nullable DialogInterface.OnClickListener onPositiveButtonClickListener,
+                                   @NonNull String positiveText,
+                                   @Nullable DialogInterface.OnClickListener onNegativeButtonClickListener,
+                                   @NonNull String negativeText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton(positiveText, onPositiveButtonClickListener);
+        builder.setNegativeButton(negativeText, onNegativeButtonClickListener);
+        mAlertDialog = builder.show();
+    }
+
+
+    private void copyFileToDownloads(Uri croppedFileUri) throws Exception {
+        String downloadsDirectoryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+        String filename = String.format("%d_%s", Calendar.getInstance().getTimeInMillis(), croppedFileUri.getLastPathSegment());
+
+        final File saveFile = new File(downloadsDirectoryPath, filename);
+
+        FileInputStream inStream = new FileInputStream(new File(croppedFileUri.getPath()));
+        FileOutputStream outStream = new FileOutputStream(saveFile);
+        FileChannel inChannel = inStream.getChannel();
+        FileChannel outChannel = outStream.getChannel();
+        inChannel.transferTo(0, inChannel.size(), outChannel);
+        inStream.close();
+        outStream.close();
+
+        Toast.makeText(this,"成功", Toast.LENGTH_LONG).show();
+
+        Glide.with(this)
+                .load(saveFile.getPath())
+                .apply(RequestOptions.centerCropTransform())
+                .into(imageView);
+
+
+/*
+        new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+                uploadImage(token, saveFile);
+                return null;
+            }
+        }.execute();
+*/
+    }
+
+    private void handleCropError(@NonNull Intent result) {
+        final Throwable cropError = UCrop.getError(result);
+        if (cropError != null) {
+            Toast.makeText(this, "cropError.getMessage() " + cropError.getMessage(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Unexpected Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onPickImage(View view) {
+        // Click on image button
+//        ImagePicker.pickImage(this, "Select your image:");
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.label_select_picture)), REQUEST_SELECT_PICTURE);
+
+    }
+
 }
