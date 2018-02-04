323696d08c9efb087db15480bea6d7430821762b
cherylgao <402850431@s02.tku.edu.tw>
2018-02-04 11:58:15 +0800

    ok

--- app/src/main/java/cheryl/fishackathon/MainActivity.java
+++ app/src/main/java/cheryl/fishackathon/MainActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView name1,name2,name3,name4,name5;
+    TextView time1,time2,time3,time4,time5;
+    TextView price1,price2,price3,price4,price5;
+    TextView address1,address2,address3,address4,address5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setHomeAsUpIndicator(R.mipmap.ic_home_account);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,CameraActivity.class));
             }
         });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+//        startActivity(new Intent(MainActivity.this,BuyActivity.class));
+        return true;
     }
 
     @Override
