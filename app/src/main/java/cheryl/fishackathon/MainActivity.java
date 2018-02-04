feda5678d3ee69e0a1ba6d6122c24255d6d702ad
cherylgao <402850431@s02.tku.edu.tw>
2018-02-04 10:04:17 +0800

    ok

--- app/src/main/java/cheryl/fishackathon/MainActivity.java
+++ app/src/main/java/cheryl/fishackathon/MainActivity.java
@@ -42,4 +42,8 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void card1OnClick(View view){
+        startActivity(new Intent(MainActivity.this,BuyActivity.class));
+    }
 }
--- app/src/main/java/cheryl/fishackathon/SellActivity.java
+++ app/src/main/java/cheryl/fishackathon/SellActivity.java
@@ -1,7 +1,9 @@
 package cheryl.fishackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SellActivity extends AppCompatActivity {
 
@@ -9,6 +11,40 @@ public class SellActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
+        textJudgement();
     }
+
+    private void textJudgement() {
+        /*
+        switch(name){
+            case "吳郭魚":
+            case "Tilapias":
+            case "Cichlids":
+            case "福壽魚":
+            case "南洋鯽":
+            case "尼羅魚":
+            case "臺灣鯛":
+            case "尼羅紅魚":
+                binomialName = "Cichlidae";
+                break;
+            case "鯁魚":
+            case "Mud carp":
+            case "鯪魚":
+            case "鯪仔魚":
+            case "花鯪仔":
+            case "青鱗魚":
+            case "土鯪":
+            case "鯪公":
+                binomialName = "Cirrhinus molitorella";
+                break;}
+                */
+    }
+
+
+    public void finishOnClick(View view){
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
 
 }
